689dffc618f1809b98448b3c27791290bf8def86
Rodrigo <r.barboza9502@gmail.com>
2023-12-13 11:46:07 -0300

    Ex3-finalized

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,27 @@
 import { Link } from "react-router-dom";
 import "./styles.css";
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
     <header>
       <div className="container">
+        <nav className="nav-main">
         <Link to={"/"}>
           <h1 className="site-name">MeuSite</h1>
         </Link>
+        <ul className="menu-items">
+          <NavLink to={"/home"} className={({isActive}) => isActive ? "menu-active" : "menu-items"}>
+              <li>Início</li>
+          </NavLink>
+          <NavLink to={"/promotion"} className={({isActive}) => isActive ? "menu-active" : "menu-items"}>
+            <li>Promoção</li>
+          </NavLink>
+          <NavLink to={"/sub"} className={({isActive}) => isActive ? "menu-active" : "menu-items"}>
+            <li>Participar</li>
+          </NavLink>
+        </ul> 
+        </nav>
       </div>
     </header>
   );
--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -4,8 +4,7 @@ import Header from "../../components/Header";
 export default function Home(){
 
     return(
-        <>
-        
+        <>  
         <Header/>
         <Outlet/>
         </>
